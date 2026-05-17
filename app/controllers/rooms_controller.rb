59b3d2c5f104cbde34032e74e215d7a25fc66806
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-05-17 15:55:29 +0900

    rooms indexのビュー・検索とルームリストの追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,5 +1,13 @@
 class RoomsController < ApplicationController
   def index
+    # 新しい施設を上に表示する
+    @rooms = Room.all.order(created_at: :desc)
+    if params[:area].present?
+      @rooms = @rooms.where("area LIKE ?", "%#{params[:area]}%")
+    end
+    if params[:keyword].present?
+     @rooms = @rooms.where("title LIKE ?", "%#{params[:keyword]}")
+    end
   end
 
   def new
