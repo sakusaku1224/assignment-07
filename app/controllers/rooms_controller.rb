5af81ed340e442f86f761652929f59742fff1793
高木桜子 <sakurakotakaki@takagisakurakonoMacBook-Air.local>
2026-05-19 15:30:44 +0900

    roomsのnewアクションとビューを記述、editビューとのパーシャルを追加

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,4 +1,5 @@
 class RoomsController < ApplicationController
+  before_action :set_room, only: [ :show, :edit, :update, :destroy ]
   def index
     # 新しい施設を上に表示する
     @rooms = Room.all.order(created_at: :desc)
@@ -6,20 +7,41 @@ class RoomsController < ApplicationController
       @rooms = @rooms.where("area LIKE ?", "%#{params[:area]}%")
     end
     if params[:keyword].present?
-     @rooms = @rooms.where("title LIKE ?", "%#{params[:keyword]}")
+     @rooms = @rooms.where("name LIKE ?", "%#{params[:keyword]}%")
     end
   end
 
   def new
+    @room = Room.new
   end
 
   def create
+    @room = Room.new(room_params)
+    if @room.save
+      redirect_to rooms_path, notice: "施設を保存しました"
+    else
+      flash.now[:alert] = "登録情報が不足しています"
+      render "new", status: :unprocessable_entity
+    end
   end
 
   def show
-    @room = Room.find(params[:id])
+    # @room実行済み
   end
 
   def own
+  end
+
+  private
+  def set_room
+    @room = Room.find(params[:id])
+  end
+  def room_params
+    params.require(:room).permit(
+      :name,
+      :description,
+      :price,
+      :address,
+      :image)
   end
 end
